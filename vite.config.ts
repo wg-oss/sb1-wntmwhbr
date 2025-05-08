636856a57fca2bf67e2f4682660872e291d7a07f
wg-oss <walker.neworleans@gmail.com>
2025-05-07 21:07:36 -0600

    re-configuring deploys

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
